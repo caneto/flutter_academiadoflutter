cfb13d81991b9a4df91576a62045a32f091a81ab
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-29 19:01:16 -0300

    Fim da Aula de Row e Column

--- flutter_primeiro_projeto/lib/pages/rows_columns/rows_columns_page.dart
+++ flutter_primeiro_projeto/lib/pages/rows_columns/rows_columns_page.dart
@@ -8,7 +8,35 @@ class RowsColumnsPage extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text('Rows & Columns'),),
-           body: Container(),
+           body: Container(
+            color: Colors.red,
+            width: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  child: const Text("Elemento 1"),
+                ),
+                const Text("Elemento 1"),
+                const Text("Elemento 1"),
+                Container(
+                  height: 200,
+                  color: Colors.amber,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const [
+                      Text('1'),
+                      Text('2'),
+                      Text('3'),
+                    ],
+                  ),
+                )
+              ]
+              ),
+           ),
        );
   }
 }
