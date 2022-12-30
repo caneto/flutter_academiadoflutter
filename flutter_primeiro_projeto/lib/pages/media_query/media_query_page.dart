ac865d37c85b9d2d77d6ac8a8c3fa54ed9d5a125
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-30 13:18:08 -0300

    Fim da Aula MediaQuery

--- flutter_primeiro_projeto/lib/pages/media_query/media_query_page.dart
+++ flutter_primeiro_projeto/lib/pages/media_query/media_query_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
 class MediaQueryPage extends StatelessWidget {
-
+  
   const MediaQueryPage({ Key? key }) : super(key: key);
 
    @override
    Widget build(BuildContext context) {
+       var mediaQuery = MediaQuery.of(context);
+
+       print('Largura ${mediaQuery.size.width}');
+       print('Altura ${mediaQuery.size.height}');
+       print('Orientação ${mediaQuery.orientation}');
+       print('Padding Top ${mediaQuery.padding.top}');
+       print('Padding Top ${kToolbarHeight}');
+
+       final statusBar = mediaQuery.padding.top;
+       final heightBody = mediaQuery.size.height - statusBar - kToolbarHeight;
+
        return Scaffold(
            appBar: AppBar(title: const Text('MidiaQuery'),),
-           body: Container(),
+           body: Center(
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: mediaQuery.size.width,
+                  height: heightBody * .5,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: mediaQuery.size.width,
+                  height: heightBody * .5,
+                ),
+              ],
+            )
+           ),
        );
   }
 }
