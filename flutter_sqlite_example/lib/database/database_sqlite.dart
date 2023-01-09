943bb20531268a539a5ddb0b4b33d8af4e39c4f8
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-09 10:49:31 -0300

    Fim da aula de Storage

--- flutter_sqlite_example/lib/database/database_sqlite.dart
+++ flutter_sqlite_example/lib/database/database_sqlite.dart
@@ -7,34 +7,84 @@ class DatabaseSqLite {
     final databasePath = await getDatabasesPath();
     final databaseFinalPath = join(databasePath, 'SQLITE_EXAMPLE');
 
-    openDatabase(
+    return await openDatabase(
       databaseFinalPath,
       version: 1,
+      onConfigure: (db) async {
+        print('onConfigure sendo chamado');
+      },
       // Chamado somente no momento de criação do banco de dados
       // primera vez que carrega o aplicativo
       onCreate: (Database db, int version) {
         print('onCreate Chamado');
         final batch = db.batch();
+
         batch.execute('''
           create table teste(
             id Integer primary key autoincrement,
             nome varchar(200),
           )
         ''');
-
+        batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200),
+            )
+          ''');
+      //  batch.execute('''
+      //      create table produto(
+      //        id Integer primary key autoincrement,
+      //        nome varchar(200),
+      //      )
+      //    ''');
         batch.commit();
       },
       // Será chamado sempre que ouver uma alteração no version incremental (1->2)
       onUpgrade: (Database db, int oldVersion, int newVersion) {
         print('onUpgrade Chamado');  
+        final batch = db.batch();
+
+        if(oldVersion == 1) {
+          batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200),
+            )
+          ''');
+        //  batch.execute('''
+        //    create table categoria(
+        //      id Integer primary key autoincrement,
+        //      nome varchar(200),
+        //    )
+        //  ''');
+        }
+
+       // if(oldVersion == 3) {
+       //   batch.execute('''
+       //     create table categoria(
+       //       id Integer primary key autoincrement,
+       //       nome varchar(200),
+       //     )
+       //   ''');
+       // }
+
+        batch.commit();
       },
       // Será chamado sempre que ouver uma alteração na version decremental (2->1)
       onDowngrade: (Database db, int oldVersion, int newVersion) {
         print('onDowngrade Chamado');
+        final batch = db.batch();
+
+        if(oldVersion == 3) {
+          batch.execute('''
+            drop table categoria
+          ''');
+        }
+
+
+        batch.commit();
+        
       }
-
-
-
     );
 
     return openConnection();
--- flutter_sqlite_example/lib/pages/home_page.dart
+++ flutter_sqlite_example/lib/pages/home_page.dart
@@ -11,11 +11,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-
-   @override
+  @override
   void initState() {
     super.initState();
-    DatabaseSqLite().openConnection();
+    _dataBase();
+  }
+
+  Future<void> _dataBase() async {
+    final database = await DatabaseSqLite().openConnection();
+
+    //database.insert('teste', {'nome': 'Carlos Alberto'});
+    //database.delete('teste', where: 'nome = ?', whereArgs: ['Carlos Alberto']);
+    //database.update('teste', {'nome': 'Curso de Fluter'}, where: 'nome = ?', whereArgs: ['Carlos Alberto']);
+
+    //var result = await database.query('teste');
+    //print(result);
+
+    //database.rawInsert('insert into teste values(null, ?)', ['Carlos Alberto']);
+    //database.rawUpdate('update teste set nome = ? where id = ?', ['Curso de Flutter', 1]);
+    //database.rawDelete('delete from teste from id = ?', [1]);
+
+    var result = await database.rawQuery('select * from teste');
+    print(result);
+
+
   }
 
    @override
