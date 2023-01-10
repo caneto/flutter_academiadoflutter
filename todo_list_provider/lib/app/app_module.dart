58754633dad1d5f8ce7504800b60d815d89a6b2b
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-10 11:18:04 -0300

    Atualização

--- todo_list_provider/lib/app/app_module.dart
+++ todo_list_provider/lib/app/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_connection_fectory.dart';
 
 class AppModule extends StatelessWidget {
   const AppModule({Key? key}) : super(key: key);
@@ -9,7 +10,10 @@ class AppModule extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider(create: (_) => Object(),)
+        Provider(
+          create: (_) => SqliteConnectionFectory(),
+          lazy: false,
+        )
       ],
       child: const AppWidget(),
     );
