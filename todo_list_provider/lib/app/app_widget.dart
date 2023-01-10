87cff8939e8dac16f63eea3bbb50cca445fdf94c
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-10 15:22:01 -0300

    Aula Configurando SQlAdminConnection

--- todo_list_provider/lib/app/app_widget.dart
+++ todo_list_provider/lib/app/app_widget.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
 
   const AppWidget({ Key? key }) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+
+  var sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
 
    @override
    Widget build(BuildContext context) {
---
+++ todo_list_provider/lib/app/core/database/sqlite_adm_connection.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_connection_fectory.dart';
+
+class SqliteAdmConnection with WidgetsBindingObserver {
+  
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final connection = SqliteConnectionFectory();
+
+    switch(state) {
+      case AppLifecycleState.resumed:
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.paused:
+      case AppLifecycleState.detached:
+        connection.closeConnection();
+        break;
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+  
+}
