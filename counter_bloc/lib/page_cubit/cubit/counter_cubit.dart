6360dbdb89c1cd8218629b1427b128878cb5bec9
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-27 10:58:54 -0300

    Aula Importancia do Equals no Bloc

--- counter_bloc/lib/page_cubit/cubit/counter_cubit.dart
+++ counter_bloc/lib/page_cubit/cubit/counter_cubit.dart
@@ -1,5 +1,6 @@
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
 part 'counter_state.dart';
 
--- counter_bloc/lib/page_cubit/cubit/counter_state.dart
+++ counter_bloc/lib/page_cubit/cubit/counter_state.dart
@@ -1,18 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'counter_cubit.dart';
 
-abstract class CounterState {
+abstract class CounterState  extends Equatable {
   final int counter;
-
   const CounterState(this.counter);
+
+  @override
+  List<Object> get props => [counter];
+
 }
 
 class CounterStateInitial extends CounterState {
-  CounterStateInitial() : super(0);
+  const CounterStateInitial() : super(0);
 
 }
 
 
 class CounterStateData extends CounterState {
-  CounterStateData(int counter) : super(counter);
+  const CounterStateData(int counter) : super(counter);
 }
