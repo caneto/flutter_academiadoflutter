52e3d589d9cb489f3e82edaa86181dd78778dd00
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-31 13:28:55 -0300

    Atualização

--- contact_bloc/lib/features/contacts/update/contact_update_page.dart
+++ contact_bloc/lib/features/contacts/update/contact_update_page.dart
@@ -1,14 +1,27 @@
+import 'package:contact_bloc/models/contact_model.dart';
 import 'package:flutter/material.dart';
 
-class ContactUpdatePage extends StatelessWidget {
+class ContactUpdatePage extends StatefulWidget {
+  final ContactModel contactModel;
+  
+  const ContactUpdatePage({Key? key, required this.contactModel}) : super(key: key);
 
-  const ContactUpdatePage({ Key? key }) : super(key: key);
+  @override
+  State<ContactUpdatePage> createState() => _ContactUpdatePageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Contact Update'),),
-           body: Container(),
-       );
+class _ContactUpdatePageState extends State<ContactUpdatePage> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController nameEC;
+  late final TextEditingController emailEC;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Update'),
+      ),
+      body: Container(),
+    );
   }
 }
