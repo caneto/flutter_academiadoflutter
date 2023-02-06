81a5bd53bc266833edffc0b7fd1feeb8c58967d2
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-02-06 17:30:07 -0300

    Aula Configurando projeto

--- cuidapet/cuidapet_mobile/lib/main.dart
+++ cuidapet/cuidapet_mobile/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
+import 'app/core/app_config.dart';
 
 Future<void> main() async {
+  await const AppConfig().configureApp();
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
