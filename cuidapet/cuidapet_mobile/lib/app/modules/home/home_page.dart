c8776100da4668468976a3a768b13a5f6e03b370
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-02-19 19:00:32 -0300

    Aula Construindo AppBar da HomePage

--- cuidapet/cuidapet_mobile/lib/app/modules/home/home_page.dart
+++ cuidapet/cuidapet_mobile/lib/app/modules/home/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../core/helpers/debouncer.dart';
 import '../../core/life_cycle/page_life_cycle_state.dart';
+import '../../core/ui/extensions/screen_size_extension.dart';
+import '../../core/ui/extensions/theme_extension.dart';
 import '../core/auth/auth_store.dart';
 import 'home_controller.dart';
+
+part 'widgets/home_appbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,12 +18,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends PageLifeCycleState<HomeController, HomePage> {
+  late ScrollController _scrollController;
+  final _containerHeight = ValueNotifier<double>(0);
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      _containerHeight.value = _scrollController.offset > 56 ? 100 : 0;
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
       drawer: SafeArea(
         child: Drawer(
           child: Column(
@@ -35,7 +55,17 @@ class _HomePageState extends PageLifeCycleState<HomeController, HomePage> {
           ),
         ),
       ),
-      body: Container(),
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (_, __) => [
+          HomeAppBar(controller),
+          //SliverToBoxAdapter(child: _HomeAddressWidget(controller: controller)),
+          //SliverToBoxAdapter(
+          //  child: _HomeCategoriesWidget(controller: controller),
+          //),
+        ],
+        body: Container(), //_HomeSupplierTab(controller: controller),
+      ),
     );
   }
 }
---
+++ cuidapet/cuidapet_mobile/lib/app/modules/home/widgets/home_appbar.dart
@@ -0,0 +1,77 @@
+part of '../home_page.dart';
+
+class HomeAppBar extends SliverAppBar {
+  HomeAppBar(
+    HomeController controller, {
+    super.key,
+    super.expandedHeight = 100,
+    super.collapsedHeight = 100,
+    super.elevation = 0,
+    super.pinned = true,
+    super.iconTheme = const IconThemeData(color: Colors.black),
+  }) : super(flexibleSpace: _CuidapetAppBar(controller: controller));
+}
+
+class _CuidapetAppBar extends StatelessWidget {
+  final HomeController controller;
+
+  const _CuidapetAppBar({required this.controller});
+
+  static final _debouncer = Debouncer(milliseconds: 500);
+
+  @override
+  Widget build(BuildContext context) {
+    final outlineInputBorder = OutlineInputBorder(
+      borderRadius: const BorderRadius.all(Radius.circular(30)),
+      borderSide: BorderSide(color: Colors.grey[200]!),
+    );
+
+    return AppBar(
+      elevation: 0,
+      centerTitle: true,
+      flexibleSpace: Stack(
+        children: [
+          Container(height: 110.h, color: context.primaryColor),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Material(
+              elevation: 4,
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+              child: SizedBox(
+                width: 0.9.sw,
+                child: TextFormField(
+                  //onChanged: (value) => _debouncer
+                  //    .run(() => controller.filterSupplierBySearchText(value)),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon:
+                        const Icon(Icons.search, size: 25, color: Colors.grey),
+                    border: outlineInputBorder,
+                    focusedBorder: outlineInputBorder,
+                    enabledBorder: outlineInputBorder,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.grey[100],
+      title: const Padding(
+        padding: EdgeInsets.only(bottom: 12),
+        child: Text('Cuidapet'),
+      ),
+      actions: [
+        IconButton(
+          onPressed: controller.goToAddress,
+          tooltip: 'Alterar endereço',
+          icon: const Icon(
+            Icons.location_on,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
