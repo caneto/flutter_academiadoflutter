a5def74d2bdd2c2e1f49276f55eeb7b472433a4f
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-03-23 15:22:25 -0300

    Atualização do TodoList

--- todo_list_provider/lib/app/core/ui/theme_extensions.dart
+++ todo_list_provider/lib/app/core/ui/theme_extensions.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 extension ThemeExtensions on BuildContext {
   Color get primaryColor => Theme.of(this).primaryColor; 
   Color get primaryColorLight => Theme.of(this).primaryColorLight; 
-  // ignore: deprecated_member_use
-  Color get buttonColor => Theme.of(this).buttonColor; 
-  TextTheme get textTheme => Theme.of(this).textTheme;
   Color get deleteColor => Theme.of(this).indicatorColor;
-
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  
 
   TextStyle get titleStyle => const TextStyle(
     fontSize: 12,
